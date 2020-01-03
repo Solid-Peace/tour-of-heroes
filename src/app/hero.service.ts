9a34dd45f2bc8778acc4541c79a86fb652d30d51
Quentin <solid@Solid-Peace.aircard>
2020-01-03 02:37:17 +0100

    Service section : Show message part start

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './Hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
 
 @Injectable({
@@ -8,8 +9,9 @@ import { HEROES } from './mock-heroes';
 })
 
 export class HeroService {
-	getHeroes(): Hero[] {
-		return HEROES;
+
+	getHeroes(): Observable<Hero[]> {
+		return of(HEROES);
 	}
 
   constructor() { }
