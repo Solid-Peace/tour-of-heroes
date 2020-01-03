e880d5b903d7eec91ce500ab121450929762ddab
Quentin <solid@Solid-Peace.aircard>
2020-01-03 02:03:17 +0100

    Service section : Observable Data part start

--- src/app/hereos/hereos.component.ts
+++ src/app/hereos/hereos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
-import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service'
 
 @Component({
   selector: 'app-hereos',
@@ -8,16 +8,22 @@ import { HEROES } from '../mock-heroes'
   styleUrls: ['./hereos.component.scss']
 })
 export class HereosComponent implements OnInit {
-  heroes = HEROES;
+
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
   	this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
